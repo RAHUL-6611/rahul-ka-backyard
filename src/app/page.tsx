8f56517d00a3f8bafd7e13f4bf30b748e2ba18d0
rahul-6611 <rahulparmar1138@gmail.com>
2024-04-15 02:37:38 +0530

    commenting doesn't work on build, so removed the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,30 +36,8 @@ export default async function Home() {
             </div>
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          {/* <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p> */}
-        </div>
-
-        {/* <CrudShowcase /> */}
       </div>
     </main>
   );
 }
 
-// async function CrudShowcase() {
-//   const latestPost = await api.post.getLatest();
-
-//   return (
-//     <div className="w-full max-w-xs">
-//       {latestPost ? (
-//         <p className="truncate">Your most recent post: {latestPost.name}</p>
-//       ) : (
-//         <p>You have no posts yet.</p>
-//       )}
-
-//       <CreatePost />
-//     </div>
-//   );
-// }
