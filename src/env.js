9371712d22be1a5d76bca517270ae9cdef66b497
rahul-6611 <rahulparmar1138@gmail.com>
2024-04-15 02:26:23 +0530

    mongodb url modified according to prisma

--- src/env.js
+++ src/env.js
@@ -11,7 +11,7 @@ export const env = createEnv({
       .string()
       .url()
       .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        (str) => !str.includes("mongodb+srv://rahulparmar1138:Rahul%40genz69@backyarddb.baspqv7.mongodb.net/?retryWrites=true&w=majority&appName=backyardDB"),
         "You forgot to change the default URL"
       ),
     NODE_ENV: z
